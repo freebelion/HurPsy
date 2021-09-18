07b95e4a4566b37eec0eacfb772c00121c477500
Hürol Aslan <freebelion@gmail.com>
2021-09-18 14:15:25 +0300

    Added first model class

---
+++ HurPsyLib/Models/StimulusModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HurPsyLib.Models
+{
+    internal class StimulusModel
+    {
+        /// <summary>
+        /// An ID string which will define a stimulus.
+        /// </summary>
+        private string id;
+
+        /// <summary>
+        /// The name of the file where the stimulus
+        /// is stored in the computer environment.
+        /// </summary>
+        private string filename;
+
+        /// <summary>
+        /// A property to get/set the id of the stimulus.
+        /// </summary>
+        public string ID
+        {
+            get { return id; }
+            set
+            {
+                id = value;
+            }
+        }
+
+        /// <summary>
+        /// A property to get/set the filename
+        /// where the stimulus object is stored.
+        /// </summary>
+        public string FileName
+        {
+            get { return filename; }
+            set
+            {
+                filename = value;
+            }
+        }
+    }
+}
