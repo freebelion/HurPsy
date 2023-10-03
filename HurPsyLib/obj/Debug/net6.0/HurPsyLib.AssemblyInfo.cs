206bca6b576f9d222a3a3a22c69a97d3ae3e50ca
Hürol Aslan <freebelion@gmail.com>
2023-10-03 17:41:24 +0300

    Rebuilt HurPsyWinForms

--- HurPsyLib/obj/Debug/net6.0/HurPsyLib.AssemblyInfo.cs
+++ HurPsyLib/obj/Debug/net6.0/HurPsyLib.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HurPsyLib")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+507adc400d9f3ca8172212f8ddd32da147923028")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0960488dc85d29f313d2e5b2028a5cb422d61d2c")]
 [assembly: System.Reflection.AssemblyProductAttribute("HurPsyLib")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HurPsyLib")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
